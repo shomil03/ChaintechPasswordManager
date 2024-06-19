f5e293aadfb36fddb8578cb86b347069bf4147f5
shomil03 <shomil02020@gmail.com>
2024-06-19 18:52:53 +0530

    Initial Commit

---
+++ ChaintechPasswordManager/ChaintechPasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaintechPasswordManagerApp.swift
+//  ChaintechPasswordManager
+//
+//  Created by Shomil Singh on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChaintechPasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
